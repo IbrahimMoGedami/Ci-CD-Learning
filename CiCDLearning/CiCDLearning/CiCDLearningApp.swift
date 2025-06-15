d899d6711dfaf9c4fe6e62d9636ecc0b31a1569a
ibrahimgedami <hemagedamy@gmail.com>
2025-06-15 11:04:22 +0400

    commit app

---
+++ CiCDLearning/CiCDLearning/CiCDLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCDLearningApp.swift
+//  CiCDLearning
+//
+//  Created by Ibrahim Gedami on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CiCDLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CiCDLearning/CiCDLearningTests/CiCDLearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  CiCDLearningTests.swift
+//  CiCDLearningTests
+//
+//  Created by Ibrahim Gedami on 15/06/2025.
+//
+
+import Testing
+@testable import CiCDLearning
+
+struct CiCDLearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
